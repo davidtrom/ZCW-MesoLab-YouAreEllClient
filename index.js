149e5388cdfb39bfa87ef568978419d63d8408f5
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-11-30 21:20:05 -0500

    added new files to GIT

---
+++ index.js
@@ -0,0 +1,81 @@
+import MessageService from "./message-service.js";
+
+let userId = "davidtrom"; //sets userid variable to "bello"
+const messageService = new MessageService(userId); //creates a new message service object
+createFormListener();
+
+window.addEventListener("load", function () {
+    document.getElementById("greeting").innerHTML = `Welcome ${userId}!`;
+    messageService.getAllMessages().then(successCallback, errorCallback);
+    
+    function successCallback() {
+        //this data comes from the resolve method?
+        populateMessages(response); //"Promise is successful!");
+    }
+    function errorCallback () {
+        //this data comes from the resolve method?
+        console.log(response); //"An error occured");
+    }
+});
+function populateMessages(messages){
+    messages.forEach(message => {
+        addMessagesToThread(message);
+    });
+}
+
+    // function populateThread(messages) {
+    //     messages.forEach(message => {
+    //         const messageListItem = document.createElement("LI");
+    //         const userIdHeading = document.createElement("h3");
+    //         const messageParagraph = document.createElement("p");
+    //         const messageContent = document.createTextNode(message.message);
+    //         const userIdContent = document.createTextNode(message.fromid);
+    //         userIdHeading.appendChild(userIdContent);
+    //         messageParagraph.appendChild(messageContent);
+    //         messageListItem
+    //             .appendChild(userIdHeading)
+    //             .appendChild(messageParagraph);
+    //         document.getElementById("message-list").appendChild(messageListItem);
+    //     })
+
+
+    function createFormListener () {
+        const form = document.getElementById("new-message-form");
+
+        form.onsubmit = function (event) {
+            event.preventDefault();
+
+            const data = {
+                fromid: userId,
+                message: form.message.value
+            };
+
+            messageService.createNewMessage(data)
+                .then(successCallback, errorCallback);
+                
+            function successCallback(response) {  //this data comes from the resolve method
+                addMessagesToThread(response);
+            }
+
+            function errorCallback(response){  //this data comes from the reject method
+                console.log(response);
+            }
+        }
+    };
+
+    function addMessagesToThread (message){
+        //messages.array.forEach(mesage => {
+            const messageListItem = document.createElement("LI");
+            const userIdHeading = document.createElement("h3");
+            const messageParagraph = document.createElement("p");
+            const messageContent = document.createTextNode(message.message);
+            const userIdContent = document.createTextNode(message.fromid);
+            userIdHeading.appendChild(userIdContent);
+            messageParagraph.appendChild(messageContent);
+            messageListItem
+                .appendChild(userIdHeading)
+                .appendChild(messageParagraph);
+            document.getElementById("message-list").appendChild(messageListItem);
+    };
+
+
---
+++ message-service.js
@@ -0,0 +1,48 @@
+export default class MessageService {
+    
+    getAllMessages (){
+        const request = new XMLHttpRequest();
+        return new Promise(function(resolve, reject){
+            request.onload = function(){    //set up our listener to process completed requests
+                if(request.status >=200 && request < 300){
+                    const threads = JSON.parse(request.responseText); //this is the returned text
+                    resolve(threads);
+                }
+                else {
+                    reject({
+                    status : request.status, //prints that an error occured during the request
+                    statusText: request.statusText
+                });
+            }
+        };
+            request.open("GET", "http://zipcode.rocks:8085/messages");
+            request.send();
+        })
+    }
+    
+    createNewMessage(message) {
+        const request = new XMLHttpRequest();
+    
+        return new Promise(function (resolve, reject) {
+            // Setup our listener to process compeleted requests
+            request.onload = function () {
+                // Process the response
+                if (request.status >= 200 && request.status < 300) {
+                    // If successful
+                    resolve(JSON.parse(request.responseText));
+                } else {
+                    reject({
+                        status: request.status,
+                        statusText: request.statusText
+                    });
+                }
+            };
+    
+            request.open("POST", `http://zipcode.rocks:8085/ids/${message.fromid}/messages`);
+    
+            request.send(JSON.stringify(message));
+        });
+    }
+
+
+}
